3ad42aacc68adfdcc10ce67dedac684691c3e1a3
JIAN <135502322+twonabi@users.noreply.github.com>
2024-02-08 15:04:26 +0900

    Initialize #1 -java project
    지난 시간 만들었던 것을 복사해왔다.
    
    #https://github.com/twonabi/fastcampus-java-oop

---
+++ src/test/java/com/fastcampus/javaoop/service/SortServiceTest.java
@@ -0,0 +1,31 @@
+package com.fastcampus.javaoop.service;
+
+import com.fastcampus.javaoop.logic.BubbleSort;
+import com.fastcampus.javaoop.logic.JavaSort;
+import com.fastcampus.javaoop.logic.Sort;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortServiceTest {
+
+    private SortService sut = new SortService(new BubbleSort<String>());
+
+
+    @Test
+    void test(){
+        //Given
+
+
+        // When
+       List<String> actual =sut.doSort(List.of("3","2","1"));
+
+        //Then
+        assertEquals(List.of("1","2","3"), actual);
+
+
+    }
+
+}
